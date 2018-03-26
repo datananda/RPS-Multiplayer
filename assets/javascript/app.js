65bf8a92ea9c6bae28832df424ca3957c6c70e89
Madeleine Filloux <mfilloux@gmail.com>
2018-03-26 17:55:09 -0500

    starter files with jquery, eslint, and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA-rzzLNb9FB4NScu03dsvE7hbPh4oisFk",
+    authDomain: "rps-multiplayer-472f4.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-472f4.firebaseio.com",
+    projectId: "rps-multiplayer-472f4",
+    storageBucket: "rps-multiplayer-472f4.appspot.com",
+    messagingSenderId: "902546356426"
+};
+
+firebase.initializeApp(config);
